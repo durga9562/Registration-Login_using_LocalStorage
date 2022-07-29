6e9bdf0ba9dca09f9f4196d9138860b88f34cea1
Durga Prasad Donga <dp21422@corp.ojas-it.com>
2022-07-29 19:36:37 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Registration from './components/Registration';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <div className="App p-5">
+      
+ <div className='outer'>
+  <div className='inner'>
+    <Registration/>
+  </div>
+ </div>
+    </div>
+  );
+}
+
+export default App;
